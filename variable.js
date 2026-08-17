31dbb3030d30d3c648ebce70645718aaea3f8f95
mohideen700 <r.s.mohideen786@gmail.com>
2026-08-17 13:25:38 +0530

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,21 @@
+var myFriend = "naveen";
+
+function greetings() {
+    return "Greetings " + myFriend + ".";
+}
+
+console.log("results: ", greetings());
+
+
+let currentYear = 2026;
+console.log("current year result:", currentYear);
+
+const lacroix1 = "applefruit";
+
+function testBestFlavor() {
+    const lacroix2 = "orange";
+
+    console.log("Ana says, lacroix " + lacroix2 + " is a better flavor than lacroix " + lacroix1 + ".");
+}
+
+testBestFlavor();
